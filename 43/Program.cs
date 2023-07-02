d6e81ba2822b695e453c3385d41c1b48220cb8f1
an2niy85 <an2niy85@mail.ru>
2023-07-02 15:50:09 +0400

    One more time

--- 43/Program.cs
+++ 43/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear();
+System.Console.WriteLine("Введите коэффициенты двух линейных уровнений (y = k1 * x + b1, y = k2 * x + b2) через запятую ( 1, 2, 3, 4)");
+string[] ratios = Console.ReadLine().Split(", ");
+if ( ratios[0] = ratios[2])
+{
+    System.Console.WriteLine("Они не пересекуться");
+    return;
+}
+int x = (ratios[3] - ratios[1])/(ratios[0] - ratios[2]);
+int y = ratios[0] * x + ratios[1];
+System.Console.WriteLine($"x = {x}, y = {y}");
