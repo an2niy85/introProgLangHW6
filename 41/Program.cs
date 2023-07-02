bb3562ab9838010150ea5fb5a6f009963a5b96f0
an2niy85 <an2niy85@mail.ru>
2023-07-02 10:53:01 +0400

    Add solutions HW6

---
+++ 41/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+System.Console.WriteLine("Введите числа через запятую ( 1, 2, 3, 4, ...): ");
+string[] numbers = Console.ReadLine().Split(", ");
+int count = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if ( int.Parse(numbers[i]) > 0)
+        count++;
+}
+System.Console.WriteLine(count);
---
+++ 45/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 45: Напишите программу,
+ которая будет создавать копию заданного массива с помощью поэлементного копирования.*/
+
+Console.Clear();
+
+int[] array = { 1, 2, 3, 4, 5 };
+
+System.Console.WriteLine($"Задан следующий массив {array} со значениями: ");
+System.Console.WriteLine(String.Join(' ', array));
+int[] copy = new int[array.Length];
+for (int i = 0; i < array.Length; i++)
+{
+    copy[i] = array[i];
+}
+System.Console.WriteLine("А вот его копия: ");
+System.Console.WriteLine(String.Join(' ', copy));
